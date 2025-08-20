e90eeaa263d615ba50428a0e1e032b3b9b3bf803
AndisKrams <523ku12i@students.codeinstitute.net>
2025-08-20 19:38:30 +0300

    Create secret key

--- meals_project/settings.py
+++ meals_project/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 import dj_database_url
 if os.path.isfile('env.py'):
-    pass
+    import env
 from pathlib import Path
 
 
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xp5!n&&oop0n0ds^=rad#qpu##x9nwr+a&7%+qc=7!2otdfe)%'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
