c278254e2cc01da1b1db934e38db4d5e7bede4c2
AndisKrams <523ku12i@students.codeinstitute.net>
2025-08-21 18:36:46 +0300

    Add views and templates for parent registration

--- meals/views.py
+++ meals/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
+from .forms import UserParentRegistrationForm
 
 # Create your views here.
 
 
 def meals(request):
     return HttpResponse("Welcome to the Meals App!")
+
+
+def register_parent(request):
+    if request.method == 'POST':
+        form = UserParentRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Registration successful. You can now log in.')
+            return redirect('login')  # Adjust to your login URL name
+    else:
+        form = UserParentRegistrationForm()
+    return render(request, 'meals/register_parent.html', {'form': form})
--- meals_project/urls.py
+++ meals_project/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from meals.views import meals
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', meals, name='meals'),  # Home page route
+    path('meals/', include('meals.urls')),
 ]
