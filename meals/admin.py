b3da694ca60680d6a071f419879c9533473ddbbb
AndisKrams <523ku12i@students.codeinstitute.net>
2025-08-21 10:31:35 +0300

    Add meal models and admin functionality

--- meals/admin.py
+++ meals/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
 
-# Register your models here.
+
+from django.contrib import admin
+from .models import Meal, MealRegistration
+
+
+@admin.register(Meal)
+class MealAdmin(admin.ModelAdmin):
+	list_display = ('name', 'description')
+
+
+@admin.register(MealRegistration)
+class MealRegistrationAdmin(admin.ModelAdmin):
+	list_display = ('date',)
+	filter_horizontal = ('meals',)
--- meals/models.py
+++ meals/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Parent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Child(models.Model):
+    parent = models.ForeignKey(
+        Parent,
+        on_delete=models.CASCADE,
+        related_name='children'
+    )
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Meal(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class MealRegistration(models.Model):
+    date = models.DateField()
+    meals = models.ManyToManyField(Meal, related_name='registrations')
+
+    def __str__(self):
+        return f"Meal Registration for {self.date}"
+
