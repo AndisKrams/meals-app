141a5f9a5194a5d6872d29bef6828099b69b26c5
AndisKrams <523ku12i@students.codeinstitute.net>
2025-09-01 10:55:18 +0100

    Create meals for day admin page

--- meals/admin.py
+++ meals/admin.py
@@ -1,7 +1,11 @@
 
 
 from django.contrib import admin
-from .models import Meal, MealRegistration
+from django.db.models import Count
+from django.urls import path
+from django.template.response import TemplateResponse
+from datetime import datetime
+from .models import Meal, MealRegistration, MealChoice
 
 
 @admin.register(Meal)
@@ -13,3 +17,48 @@ class MealAdmin(admin.ModelAdmin):
 class MealRegistrationAdmin(admin.ModelAdmin):
     list_display = ('date',)
     filter_horizontal = ('meals',)
+
+
+@admin.register(MealChoice)
+class MealChoiceAdmin(admin.ModelAdmin):
+    list_display = ('child', 'meal', 'meal_registration')
+    list_filter = ('child__year_group', 'meal', 'meal_registration__date')
+    search_fields = ('child__first_name', 'child__last_name')
+
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path('meals-for-day/', self.admin_site.admin_view(self.meals_for_day), name='meals-for-day'),
+        ]
+        return my_urls + urls
+
+    def meals_for_day(self, request):
+        # Get the available dates that have meal choices
+        available_dates = MealChoice.objects.order_by('meal_registration__date').values_list('meal_registration__date', flat=True).distinct()
+
+        date_str = request.GET.get('date')
+        if date_str:
+            try:
+                date = datetime.strptime(date_str, '%Y-%m-%d').date()
+            except ValueError:
+                date = None
+        else:
+            date = available_dates.first() if available_dates else None
+
+        if date:
+            meal_registrations = MealRegistration.objects.filter(date=date)
+            meals = MealChoice.objects.filter(meal_registration__in=meal_registrations).order_by('child__year_group', 'child__last_name')
+            meal_totals = meals.values('meal__name').annotate(total=Count('id')).order_by('-total')
+        else:
+            meals = []
+            meal_totals = []
+
+        context = {
+            'title': f'Meals for {date}' if date else 'Meals for Day',
+            'meals': meals,
+            'meal_totals': meal_totals,
+            'date': date,
+            'available_dates': available_dates,
+        }
+
+        return TemplateResponse(request, 'admin/meals_for_day.html', context)
--- meals_project/urls.py
+++ meals_project/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from meals import views
 from django.contrib.auth import views as auth_views
+from meals.admin import MealChoiceAdmin
+from meals.models import MealChoice
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -34,6 +36,7 @@ urlpatterns = [
         views.delete_meal_choice,
         name='delete_meal_choice'
     ),
+    path('meals-for-day/', admin.site.admin_view(MealChoiceAdmin(MealChoice, admin_site=admin.site).meals_for_day), name='meals-for-day'),
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name='meals/password_reset.html'), name='password_reset'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='meals/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='meals/password_reset_confirm.html'), name='password_reset_confirm'),
