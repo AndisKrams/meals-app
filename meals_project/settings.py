37f32ea1694161773bf5e274e93d7cf07b47edbe
AndisKrams <523ku12i@students.codeinstitute.net>
2025-12-11 10:50:56 +0000

    Set debug in env.py

--- meals_project/settings.py
+++ meals_project/settings.py
@@ -28,7 +28,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEVELOPMENT") in os.environ
 
 ALLOWED_HOSTS = [
     '.herokuapp.com',
